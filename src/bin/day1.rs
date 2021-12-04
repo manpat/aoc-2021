7ca0d957b88f7990e6a71d247c584bd6aa4e33c0
Patrick Monaghan <0x5f.manpat@gmail.com>
2021-12-04 00:53:17 +0000

    day 1 complete

---
+++ src/bin/day1.rs
@@ -0,0 +1,26 @@
+#![feature(type_ascription, slice_group_by)]
+
+
+fn main() {
+	// part 1
+	let input = include_str!("day1.txt").lines()
+		.filter_map(|l| l.parse().ok())
+		.collect(): Vec<i32>;
+
+	let num_increases = input.group_by(|&a, &b| a < b)
+		.map(|slice| slice.len()-1).sum(): usize;
+
+	dbg!(num_increases);
+
+	// part 2
+	let sums = input.windows(3)
+		.map(|slice| slice.iter().cloned().sum(): i32)
+		.collect(): Vec<_>;
+
+
+	let num_increases = sums.group_by(|&a, &b| a < b)
+		.map(|slice| slice.len()-1).sum(): usize;
+
+	dbg!(num_increases);
+
+}
